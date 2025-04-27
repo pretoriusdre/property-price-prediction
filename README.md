a6b7d0a34b00b5d19870fd00159848562b4ef206
Andre Pretorius <pretorius.dre+github@gmail.com>
2025-04-27 11:04:37 +0800

    Add sensitivities

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ Each row should represent a past sale and include the following columns:
 - `car` – number of car spaces  
 - `land` – land size in square meters  
 - `price` – final sale price  
-- `date` – sale date in `YYYY-MM-DD` format  
+- `date` – sale date in an Excel date format
 - `desirability` – *(optional)* subjective score from 1–10 which accounts for intangible qualities like the building condition, street, etc. Set to a constant number eg 7 if you don't want to score.
 - `url` – *(optional)* A link to the datasource
 - `comments` – *(optional)* Notes about the property
@@ -55,6 +55,7 @@ interface.ipynb
 
 
 ### Sample prediction input:
+Input:
 
 ```python
 predictor.predict(
@@ -65,17 +66,34 @@ predictor.predict(
         'car': 2,
         'land': 750,
         'desirability': 8,
-        'date': '2025-04-20'
+        'date': '2025-04-20'    # Enter as 'YYYY-MM-DD'
     }
 )
 ```
-
-_Predicted price for the house with parameters {'data_model': 'suburb1', 'bed': 3, 'bath': 2, 'car': 2, 'land': 750, 'desirability': 8, 'date': '2025-04-20'}:
-$1,174,627_
-
+Output:
+```
+Predicted price for the house with parameters {'data_model': 'suburb1', 'bed': 3, 'bath': 2, 'car': 2, 'land': 750, 'desirability': 8, 'date': '2025-04-20'}:
+$1,174,627
+```
 
 ---
+### Sample sensitivity evaluation:
+Input:
+```python
+predictor.get_sensitivities(model_name='suburb1')
+```
 
+Output:
+
+    ```
+    2025-04-27 10:57:45,629 - INFO - Sensitivities for suburb1:
+    2025-04-27 10:57:45,629 - INFO - Impact of ± 1 bedroom: $34,110
+    2025-04-27 10:57:45,630 - INFO - Impact of ± 1 bathroom: $63,929
+    2025-04-27 10:57:45,630 - INFO - Impact of ± 1 car bay: $-5,355
+    2025-04-27 10:57:45,631 - INFO - Impact of ± 100 sqm land: $56,033
+    2025-04-27 10:57:45,632 - INFO - Impact of ± 1 month: $9,754
+    2025-04-27 10:57:45,632 - INFO - Impact of ± 1 desirability point: $66,572
+    ```
 
 ## 💡 Contibuting:
 If you want to contribute improvements to this, please raise a PR.
--- property_price_prediction/predictor.py
+++ property_price_prediction/predictor.py
@@ -5,7 +5,11 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from typing import Dict, Any, Optional
 
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 class PropertyPricePredictor:
@@ -13,7 +17,7 @@ class PropertyPricePredictor:
     EPOCH_DATE = datetime(2000, 1, 1)  # Used to convert dates into integer number of days since
     FEATURES_TO_USE = ['bed', 'bath', 'car', 'land', 'days_since_epoch', 'desirability']
 
-    def __init__(self, data_path=None):
+    def __init__(self, data_path: Optional[str] = None) -> None:
         if not data_path:
             try:
                 data_path = 'data/similar_sales_private.xlsx'
@@ -30,20 +34,43 @@ class PropertyPricePredictor:
 
 
     @staticmethod
-    def get_days_since_epoch(date):
+    def get_days_since_epoch(date: str | datetime) -> int:
         return (pd.to_datetime(date) - PropertyPricePredictor.EPOCH_DATE).days
 
 
-    def load_data(self):
-        self.df = pd.read_excel(self.data_path)
+    def load_data(self) -> pd.DataFrame:
+
+        try:
+            self.df = pd.read_excel(self.data_path)
+        except FileNotFoundError as e:
+            logger.error(e)
+            raise e
+
         self.df['days_since_epoch'] = self.df['date'].apply(self.get_days_since_epoch)
         return self.df
 
 
-    def build_models(self):
+    def build_models(self) -> Dict[str, LinearRegression]:
         
         for model_name in self.df['data_model'].unique():
-            df_subset = self.df[self.df['data_model'] == model_name]
+
+            logger.info(f'Starting on {model_name}')
+
+            df_subset = self.df[self.df['data_model'] == model_name].copy()
+
+            missing_features = df_subset[self.FEATURES_TO_USE].isnull().sum()
+            
+            if missing_features.any():
+                logger.warning(f'Missing values detected in feature columns for model "{model_name}":')
+                logger.warning(f"Number of missing values in each column: {dict(missing_features[missing_features > 0])}")
+
+                for feature in self.FEATURES_TO_USE:
+                    if df_subset[feature].isnull().any():
+                        mode_value = df_subset[feature].mode()[0]
+                        df_subset[feature] = df_subset[feature].fillna(mode_value)
+                        logger.warning(f'Filled missing values in feature "{feature}" with the most common value. Accuracy is affected.')
+                                    
+
             x = df_subset[self.FEATURES_TO_USE]
             y = df_subset['price']
 
@@ -56,10 +83,11 @@ class PropertyPricePredictor:
             r2 = r2_score(y_test, y_pred)
             rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
-            print(model_name)
-            print(f'Number of datapoints used for training: {len(df_subset)}')
-            print(f'R² score: {r2:.4f}')
-            print(f'RMS error: ${rmse:,.0f}')
+            
+            
+            logger.info(f'Number of datapoints used for training: {len(df_subset)}')
+            logger.info(f'R² score: {r2:.4f}')
+            logger.info(f'RMS error: ${rmse:,.0f}')
             print('------------')
 
             self.models[model_name] = model
@@ -67,14 +95,42 @@ class PropertyPricePredictor:
         return self.models
 
 
-    def predict(self, parameters: dict):
+    def predict(self, parameters: Dict[str, Any]) -> int:
         model_name = parameters['data_model']
         if model_name not in self.models:
-            raise ValueError(f"No model found for '{model_name}'. Did you train the models?")
+            raise ValueError(f'No model found for "{model_name}". Did you include the model in the sample data?')
 
         test_house = pd.DataFrame([parameters])
         test_house['days_since_epoch'] = test_house['date'].apply(self.get_days_since_epoch)
 
         prediction = self.models[model_name].predict(test_house[self.FEATURES_TO_USE])
-        print(f'Predicted price for the house with parameters {parameters}:\n${prediction[0]:,.0f}')
+        logger.info(f'Predicted price for the house with parameters {parameters}:\n${prediction[0]:,.0f}')
         return int(round(prediction[0], 0))
+
+
+
+    def get_sensitivities(self, model_name: str) -> None:
+
+        model = self.models[model_name]
+        bed_coef = model.coef_[self.FEATURES_TO_USE.index('bed')]
+        bath_coef = model.coef_[self.FEATURES_TO_USE.index('bath')]
+        car_coef = model.coef_[self.FEATURES_TO_USE.index('car')]
+        land_coef = model.coef_[self.FEATURES_TO_USE.index('land')]
+        date_coef = model.coef_[self.FEATURES_TO_USE.index('days_since_epoch')]
+        desirability_coef = model.coef_[self.FEATURES_TO_USE.index('desirability')]
+
+        impact_bed = bed_coef * 1 # Impact of 1 more bedroom
+        impact_car = car_coef
+        impact_bath = bath_coef
+        impact_land = land_coef * 100  # 100 sqm increase
+        impact_date = date_coef * 30  # 1 month
+        impact_desirability = desirability_coef
+
+        logger.info(f'Sensitivities for {model_name}:')
+        logger.info(f'Impact of ± 1 bedroom: ${impact_bed:,.0f}')
+        logger.info(f'Impact of ± 1 bathroom: ${impact_bath:,.0f}')
+        logger.info(f'Impact of ± 1 car bay: ${impact_car:,.0f}')
+        logger.info(f'Impact of ± 100 sqm land: ${impact_land:,.0f}')
+        logger.info(f'Impact of ± 1 month: ${impact_date:,.0f}')
+        logger.info(f'Impact of ± 1 desirability point: ${impact_desirability:,.0f}')
+
